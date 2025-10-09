495a9e41fae64dd5f05fe6dd76605f5aac115961
PhamQuang2600 <100197752+PhamQuang2600@users.noreply.github.com>
2025-10-09 15:10:26 +0700

    result conflict

--- lib/test.dart
+++ lib/test.dart
@@ -2,7 +2,8 @@ import 'dart:io';
 
 void main() async {
   // Kết nối tới server WebSocket (ví dụ server echo test của Dart)
-  var socket = await WebSocket.connect('s://echo.websocket.events');
+
+  var socket = await WebSocket.connect('wss://echo.websocket.event');
 
   // Lắng nghe tin nhắn (single-subscription stream)
   socket.listen((message) {
